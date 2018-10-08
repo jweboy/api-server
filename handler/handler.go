e3b6c0e3276c6ad41898fac085039f1924d4cc1a
jweboy <1259524459@qq.com>
2018-10-08 23:17:29 +0800

    Add User API

---
+++ handler/handler.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"net/http"
+
+	"restful-api-server/pkg/errno"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func SendResponse(c *gin.Context, err error, data interface{}) {
+	code, message := errno.DecodeErr(err)
+
+	// always return http.statusOK
+	c.JSON(http.StatusOK, Response{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	})
+}
