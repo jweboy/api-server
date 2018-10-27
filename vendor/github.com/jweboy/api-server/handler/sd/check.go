36934131347d59c9d4b055ba495adbe77cbf97ac
jweboy <1259524459@qq.com>
2018-10-27 17:31:33 +0800

    Add local dep in vendor dir

---
+++ vendor/github.com/jweboy/api-server/handler/sd/check.go
@@ -0,0 +1,96 @@
+package sd
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/load"
+	"github.com/shirou/gopsutil/mem"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	B  = 1
+	KB = 1024 * B
+	MB = 1024 * KB
+	GB = 1024 * MB
+)
+
+func HealthCheck(c *gin.Context) {
+	message := "ok"
+	c.String(http.StatusOK, "\n"+message)
+}
+
+func DiskCheck(c *gin.Context) {
+	u, _ := disk.Usage("/")
+
+	usedMB := int(u.Used) / MB
+	usedGB := int(u.Used) / GB
+	totalMB := int(u.Total) / MB
+	totalGB := int(u.Total) / GB
+	usedPercent := int(u.UsedPercent)
+
+	status := http.StatusOK
+	text := "ok"
+
+	if usedPercent >= 95 {
+		status = http.StatusOK
+		text = "CRITCAL"
+	} else if usedPercent >= 90 {
+		status = http.StatusTooManyRequests
+		text = "WARNING"
+	}
+
+	message := fmt.Sprintf("%s - Free space: %dMB(%dGB) / %dMB(%dGB) | Used: %d%%", text, usedMB, usedGB, totalMB, totalGB, usedPercent)
+	c.String(status, "\n"+message)
+}
+
+func CPUCheck(c *gin.Context) {
+	cores, _ := cpu.Counts(false)
+
+	a, _ := load.Avg()
+	l1 := a.Load1
+	l5 := a.Load15
+	l15 := a.Load15
+
+	status := http.StatusOK
+	text := "ok"
+
+	if l5 >= float64(cores-1) {
+		status = http.StatusInternalServerError
+		text = "CRITCAL"
+	} else if l5 >= float64(cores-2) {
+		status = http.StatusTooManyRequests
+		text = "WARNING"
+	}
+
+	message := fmt.Sprintf("%s - Load average: %.2f, %.2f, %.2f | Cores: %d", text, l1, l5, l15, cores)
+	c.String(status, "\n"+message)
+}
+
+func RAMCheck(c *gin.Context) {
+	u, _ := mem.VirtualMemory()
+
+	usedMB := int(u.Used) / MB
+	usedGB := int(u.Used) / GB
+	totalMB := int(u.Total) / MB
+	totalGB := int(u.Total) / GB
+	usedPercent := int(u.UsedPercent)
+
+	status := http.StatusOK
+	text := "ok"
+
+	if usedPercent >= 95 {
+		status = http.StatusInternalServerError
+		text = "CRITCAL"
+	} else if usedPercent >= 90 {
+		status = http.StatusTooManyRequests
+		text = "WARNING"
+	}
+
+	message := fmt.Sprintf("%s - Free space: %dMB (%dGB) / %dMB (%dGB) | Used: %d%%", text, usedMB, usedGB, totalMB, totalGB, usedPercent)
+	c.String(status, "\n"+message)
+}
