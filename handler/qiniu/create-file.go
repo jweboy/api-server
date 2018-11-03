5809a0784bc6bca2b5e16741f81e83cde6b04b3f
jweboy <1259524459@qq.com>
2018-11-03 18:01:06 +0800

    Add POST /file API with database

--- handler/qiniu/create-file.go
+++ handler/qiniu/create-file.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	. "github.com/jweboy/api-server/handler"
+	"github.com/jweboy/api-server/model"
 	"github.com/jweboy/api-server/pkg/errno"
 	"github.com/qiniu/api.v7/auth/qbox"
 	"github.com/qiniu/api.v7/storage"
@@ -24,10 +25,10 @@ import (
 // @Router /qiniu/file/{bucketName} [post]
 func UploadFile(c *gin.Context) {
 	// TODO: 文件大小需要作限制
+	// TODO: 请求参数校验整理
 
-	// 检查对应的存储空间名是否上传
-	bucketName := c.Param("bucketName")
-	if bucketName == "" {
+	bucket := c.Param("bucket")
+	if bucket == "" {
 		SendResponse(c, errno.ErrBind, nil)
 		return
 	}
@@ -75,25 +76,41 @@ func UploadFile(c *gin.Context) {
 	}
 
 	// 获取上传的token
-	upToken := getToken(bucketName)
+	// TODO: 七牛云有一个getToken的方法可以替换
+	upToken := getToken(bucket)
 
 	// 构建表单上传的对象
 	formUploader := storage.NewFormUploader(&cfg)
 
-	// 上传文件
+	// 上传文件到七牛云
 	// FIXME: 由于目前对于文件流操作不熟悉，暂时采用上传到服务器之后再传一份到七牛云服务器，后期优化为数据流上传的方式。
-	errs := formUploader.PutFile(context.Background(), &putRet, upToken, "image/"+fileName, "files/"+fileName, &putExtra)
+	errs := formUploader.PutFile(context.Background(), &putRet, upToken, fileName, "files/"+fileName, &putExtra)
 	if errs != nil {
 		fmt.Println("errs:", errs.Error())
 		SendResponse(c, errno.ErrFileUpload, nil)
 		return
 	}
 
-	// TODO: 考虑增加一步数据库的入库操作
+	// 存入数据库的数据模型
+	f := model.FileModel{
+		Name: putRet.Key,
+		Key:  putRet.Hash,
+	}
+
+	// TODO: 数据库重复存入的问题
+	// if err := f.Find(); err != nil {
+	// 	fmt.Printf("Database find error => %v", err)
+	// }
+
+	// 存入数据库
+	if errs := f.Create(); errs != nil {
+		fmt.Printf("Database create error => %v", errs)
+		SendResponse(c, errno.ErrDatabase, nil)
+		return
+	}
 
 	// 返回成功结果
-	SendResponse(c, nil, putRet)
-
+	SendResponse(c, nil, nil)
 }
 
 // ListFile 获取指定空间的文件列表
