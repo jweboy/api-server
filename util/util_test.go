d22bdcabbf8f6ec23b8542f0a3b76bc22bc45e26
jweboy <1259524459@qq.com>
2018-12-22 14:38:33 +0800

    :fire: Comment code that is not used

--- util/util_test.go
+++ util/util_test.go
@@ -1,37 +1,37 @@
 package util
 
-import "testing"
+// import "testing"
 
-func TestGenShortId(t *testing.T) {
-	shortID, err := GenShortId()
+// func TestGenShortId(t *testing.T) {
+// 	shortID, err := GenShortId()
 
-	if shortID == "" || err != nil {
-		t.Error("GenShortId failed.")
-	}
+// 	if shortID == "" || err != nil {
+// 		t.Error("GenShortId failed.")
+// 	}
 
-	t.Log("GenShortId test 	pass.")
-}
+// 	t.Log("GenShortId test 	pass.")
+// }
 
-func BenchmarkGenShortId(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		GenShortId()
-	}
-}
+// func BenchmarkGenShortId(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		GenShortId()
+// 	}
+// }
 
-func BenchmarkGenShortIdTimeConsuming(b *testing.B) {
-	// 停止测试时间
-	b.StopTimer()
+// func BenchmarkGenShortIdTimeConsuming(b *testing.B) {
+// 	// 停止测试时间
+// 	b.StopTimer()
 
-	shortID, err := GenShortId()
+// 	shortID, err := GenShortId()
 
-	if shortID == "" || err != nil {
-		b.Error(err)
-	}
+// 	if shortID == "" || err != nil {
+// 		b.Error(err)
+// 	}
 
-	// 重新开始时间
-	b.StartTimer()
+// 	// 重新开始时间
+// 	b.StartTimer()
 
-	for i := 0; i < b.N; i++ {
-		GenShortId()
-	}
-}
+// 	for i := 0; i < b.N; i++ {
+// 		GenShortId()
+// 	}
+// }
